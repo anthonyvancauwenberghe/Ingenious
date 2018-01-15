519eb4bec7af5bd19f6ac552a7ea75587d662b9c
Lianne Hufkens <lianne_hufkens@yahoo.com>
2018-01-15 16:38:27 +0100

    Piece names.

--- src/com/ingenious/model/Piece.java
+++ src/com/ingenious/model/Piece.java
@@ -4,31 +4,31 @@ package com.ingenious.model;
 public class Piece {
 
     public final static Piece RED_BLUE = new Piece(Tile.red, Tile.blue,"RED_BLUE");
-    public final static Piece RED_GREEN = new Piece(Tile.red, Tile.green);
-    public final static Piece RED_ORANGE = new Piece(Tile.red, Tile.orange);
-    public final static Piece RED_YELLOW = new Piece(Tile.red, Tile.yellow);
-    public final static Piece RED_PURPLE = new Piece(Tile.red, Tile.purple);
-    public final static Piece RED_RED = new Piece(Tile.red, Tile.red);
+    public final static Piece RED_GREEN = new Piece(Tile.red, Tile.green, "RED_GREEN");
+    public final static Piece RED_ORANGE = new Piece(Tile.red, Tile.orange, "RED_ORANGE");
+    public final static Piece RED_YELLOW = new Piece(Tile.red, Tile.yellow, "RED_YELLOW");
+    public final static Piece RED_PURPLE = new Piece(Tile.red, Tile.purple, "RED_PURPLE");
+    public final static Piece RED_RED = new Piece(Tile.red, Tile.red, "RED_RED");
 
-    public final static Piece BLUE_GREEN = new Piece(Tile.blue, Tile.green);
-    public final static Piece BLUE_ORANGE = new Piece(Tile.blue, Tile.orange);
-    public final static Piece BLUE_YELLOW = new Piece(Tile.blue, Tile.yellow);
-    public final static Piece BLUE_PURPLE = new Piece(Tile.blue, Tile.purple);
-    public final static Piece BLUE_BLUE = new Piece(Tile.blue, Tile.blue);
+    public final static Piece BLUE_GREEN = new Piece(Tile.blue, Tile.green, "BLUE_GREEN");
+    public final static Piece BLUE_ORANGE = new Piece(Tile.blue, Tile.orange, "BLUE_ORANGE");
+    public final static Piece BLUE_YELLOW = new Piece(Tile.blue, Tile.yellow, "BLUE_YELLOW");
+    public final static Piece BLUE_PURPLE = new Piece(Tile.blue, Tile.purple, "BLUE_PURPLE");
+    public final static Piece BLUE_BLUE = new Piece(Tile.blue, Tile.blue, "BLUE_BLUE");
 
-    public final static Piece GREEN_ORANGE = new Piece(Tile.green, Tile.orange);
-    public final static Piece GREEN_YELLOW = new Piece(Tile.green, Tile.yellow);
-    public final static Piece GREEN_PURPLE = new Piece(Tile.green, Tile.purple);
-    public final static Piece GREEN_GREEN = new Piece(Tile.green, Tile.green);
+    public final static Piece GREEN_ORANGE = new Piece(Tile.green, Tile.orange, "GREEN_ORANGE");
+    public final static Piece GREEN_YELLOW = new Piece(Tile.green, Tile.yellow, "GREEN_YELLOW");
+    public final static Piece GREEN_PURPLE = new Piece(Tile.green, Tile.purple, "GREEN_PURPLE");
+    public final static Piece GREEN_GREEN = new Piece(Tile.green, Tile.green, "GREEN_GREEN");
 
-    public final static Piece ORANGE_YELLOW = new Piece(Tile.orange, Tile.yellow);
-    public final static Piece ORANGE_PURPLE = new Piece(Tile.orange, Tile.purple);
-    public final static Piece ORANGE_ORANGE = new Piece(Tile.orange, Tile.orange);
+    public final static Piece ORANGE_YELLOW = new Piece(Tile.orange, Tile.yellow, "ORANGE_YELLOW");
+    public final static Piece ORANGE_PURPLE = new Piece(Tile.orange, Tile.purple, "ORANGE_PURPLE");
+    public final static Piece ORANGE_ORANGE = new Piece(Tile.orange, Tile.orange, "ORANGE_ORANGE");
 
-    public final static Piece YELLOW_PURPLE = new Piece(Tile.yellow, Tile.purple);
-    public final static Piece YELLOW_YELLOW = new Piece(Tile.yellow, Tile.yellow);
+    public final static Piece YELLOW_PURPLE = new Piece(Tile.yellow, Tile.purple, "YELLOW_PURPLE");
+    public final static Piece YELLOW_YELLOW = new Piece(Tile.yellow, Tile.yellow, "YELLOW_YELLOW");
 
-    public final static Piece PURPLE_PURPLE = new Piece(Tile.purple, Tile.purple);
+    public final static Piece PURPLE_PURPLE = new Piece(Tile.purple, Tile.purple, "PURPLE_PURPLE");
 
     private Tile head;
     private Tile tail;
